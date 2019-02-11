bee66e51e08ff0b6b2cde7cd33455394123f6988
chmaraj <john.chmara@mail.mcgill.ca>
2019-02-11 07:58:05 -0500

    Create Page.java

---
+++ CFIAssembly/Page.java
@@ -0,0 +1,14 @@
+package bacterialgenomeassembly;
+
+public interface Page {
+
+	public void run();
+	
+//	public String[] getOptions();
+	
+//	public void setOptions(String[] options);
+	
+//	public String[] getUserDefined();
+	
+//	public void setUserDefined(String[] userDefined);
+}
